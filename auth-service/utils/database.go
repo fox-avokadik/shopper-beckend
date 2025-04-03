4ba63f3716233424bd49ee607e8de55ab61351d6
Stepan Bezhuk <sbezhuk@icloud.com>
2025-04-03 17:06:38 +0300

    fix: remove pg_cron

--- auth-service/utils/database.go
+++ auth-service/utils/database.go
@@ -63,7 +63,6 @@ func NewGormDB() (*gorm.DB, error) {
 func AutoMigrate(db *gorm.DB) error {
 	return db.Exec(`
         CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-        CREATE EXTENSION IF NOT EXISTS "pg_cron";
         
         CREATE TABLE IF NOT EXISTS users (
             id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -100,9 +99,5 @@ func AutoMigrate(db *gorm.DB) error {
         CREATE INDEX IF NOT EXISTS idx_refresh_tokens_user_id ON refresh_tokens (user_id);
         CREATE INDEX IF NOT EXISTS idx_refresh_tokens_expires_at ON refresh_tokens (expires_at);
         
-        SELECT cron.schedule(
-            '0 * * * *',
-            $$DELETE FROM refresh_tokens WHERE is_revoked = TRUE$$
-        );
     `).Error
 }
